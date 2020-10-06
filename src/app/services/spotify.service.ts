9f42b831c8d5ec9e69022f88988ee8285d9ccd34
Jose Luis <jluisvazquez.jv@gmail.com>
2020-10-06 10:28:26 -0500

    101 Centralizar las peticiones hacia Spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,23 +12,25 @@ export class SpotifyService {
     console.log('spotify listo');
   }
 
-  getNewReleases() {
+  getQuery( query: string ) {
+    const url = `https://api.spotify.com/v1/${ query }`;
 
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQBu1kS6QJMsc2ITFRyy9v731-TrOOefpTZxZtVSNk5NJj6AHB1S_yXIquY6BcnGBl9utigqI7Z_ZAf1iKJ0JM-Op_O7OewQrnIiMspGYF9Xy4kHamP-EWfrS5LG1b7gYiYX1zjCSUs0Wn37xYCI0dL9H2osE2I'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases() {
+
+    return this.getQuery('browse/new-releases')
         .pipe( map( data => data['albums'].items ));
   }
 
   getArtista( termino: string ) {
 
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBu1kS6QJMsc2ITFRyy9v731-TrOOefpTZxZtVSNk5NJj6AHB1S_yXIquY6BcnGBl9utigqI7Z_ZAf1iKJ0JM-Op_O7OewQrnIiMspGYF9Xy4kHamP-EWfrS5LG1b7gYiYX1zjCSUs0Wn37xYCI0dL9H2osE2I'
-    });
-
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers })
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
         .pipe( map( data => data['artists'].items ));
   }
 }
