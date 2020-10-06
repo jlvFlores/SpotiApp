329e6d99550bad309a4395ef9abdab516f6e92d5
Jose Luis <jluisvazquez.jv@gmail.com>
2020-10-06 12:30:13 -0500

    106 obtener artista de Spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -28,9 +28,15 @@ export class SpotifyService {
         .pipe( map( data => data['albums'].items ));
   }
 
-  getArtista( termino: string ) {
+  getArtistas( termino: string ) {
 
     return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
         .pipe( map( data => data['artists'].items ));
+  }
+
+  getArtista( id: string ) {
+
+    return this.getQuery(`artists/${ id }`)
+      // .pipe( map( data => data['artist'].items ));
   }
 }
