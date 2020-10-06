ae1245ef58fe97a1fa51848104483349077094ae
Jose Luis <jluisvazquez.jv@gmail.com>
2020-10-06 10:07:51 -0500

    99 componente de busqueda de artistas

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -18,4 +18,13 @@ export class SpotifyService {
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
   }
+
+  getArtista( termino: string ) {
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQBu1kS6QJMsc2ITFRyy9v731-TrOOefpTZxZtVSNk5NJj6AHB1S_yXIquY6BcnGBl9utigqI7Z_ZAf1iKJ0JM-Op_O7OewQrnIiMspGYF9Xy4kHamP-EWfrS5LG1b7gYiYX1zjCSUs0Wn37xYCI0dL9H2osE2I'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
+  }
 }
